3fc2e31f0bd20fee92b011cd862bf3b429415f07
budleigh@zagato <cz@signicode.com>
2017-06-17 21:25:53 +0200

    test case

--- test/v1/plugin-test.js
+++ test/v1/plugin-test.js
@@ -10,7 +10,10 @@ const {DataStream, StringStream} = require(process.env.SCRAMJET_TEST_HOME || '..
                 cnt++;
                 this[xSymbol] = xSymbol;
             },
-            addX
+            addX,
+            get test() {
+                return xSymbol;
+            }
         },
         StringStream: {
             constructor({xSymbol: optionsX}) {
@@ -25,7 +28,7 @@ const {DataStream, StringStream} = require(process.env.SCRAMJET_TEST_HOME || '..
 );
 
 exports.test_plugin = (test) => {
-    test.expect(5);
+    test.expect(6);
     cnt = 0;
 
     const stream = DataStream.fromArray([1,2,3,4,5,6,7,8,9,10]);
@@ -34,6 +37,7 @@ exports.test_plugin = (test) => {
     test.equals(stream[xSymbol], xSymbol, "DataStream must be extended");
     test.equals(stream2.x, xSymbol, "StringStream must be replaced by constructor");
     test.equals(stream.addX, addX, "DataStream must have the plugin method");
+    test.equals(stream.test, xSymbol, "DataStream must have the plugin getter");
     test.equals(cnt, 2, "DataStream constructor must be called before StringStream");
     stream.addX(test);
 
