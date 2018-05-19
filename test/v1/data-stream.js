d40693d3b7cb4a72d42b9d65424d6678fa7b6ad4
Michał@Vulcan <cz@fuugo.com>
2018-05-19 23:16:59 +0200

    added tests for DataStream.from

--- test/v1/data-stream.js
+++ test/v1/data-stream.js
@@ -1,4 +1,5 @@
 const {DataStream} = require(process.env.SCRAMJET_TEST_HOME || '../../');
+const {PassThrough} = require("stream");
 
 const getStream = () => {
     const ret = new DataStream();
@@ -37,6 +38,20 @@ module.exports = {
             ;
 
             test.ok(notDone, "Does not resolve before the stream ends");
+        }
+    },
+    test_from: {
+        async noOptions(test) {
+            const x = new PassThrough({objectMode: true});
+            x.write(1);
+            x.write(2);
+            x.end(3);
+
+            const z = await DataStream.from(x)
+                .toArray();
+
+            test.deepEqual([1,2,3], z, "Should be the same as the stream");
+            test.done();
         }
     },
     test_options: {
