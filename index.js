8153a82e26524a08d302dcc89a3215ffb5a59c89
Budleigh Salterton driving a Miura <budleigh.salterton@gmail.com>
2019-02-25 12:37:25 +0100

    Move transpiled lib to separate folder

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const ver = process.version.slice(1).split(".");
 
-module.exports = require(+ver[0] > 8 && +ver[1] > 3 ? "./lib/index" : "./lib-6/index");
+module.exports = require(+ver[0] > 8 && +ver[1] > 3 ? "./lib/index" : "./v6/lib/index");
