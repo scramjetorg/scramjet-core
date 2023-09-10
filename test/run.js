53ccfa71a2ed54ccf0c386521b24e85f78a94f57
Budleigh Salterton <cz@scramjet.org>
2023-09-10 17:57:20 +0000

    Fix while/until test issue

--- test/run.js
+++ test/run.js
@@ -6,4 +6,5 @@ const path = require("path");
 DataStream
     .fromArray([{path: path.resolve(process.cwd(), process.argv[2])}])
     .use("nodeunit-tape-compat", {timeout: 10000})
+    .catch(e => console.error(e))
 ;
